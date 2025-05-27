f7b6f9db863b8f00debfdd39cbf32f60e5af8e2c
Mirfarrux <asadovasad0636@gmail.com>
2025-05-27 21:56:46 +0500

    main boshlandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            
-          ],
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF0171B1), Color(0xFF001645)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Travel",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+                  ),
+                  SvgPicture.asset("assets/svgs/icon1.svg"),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
-}
+  }
 }
